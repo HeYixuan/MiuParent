e2593bd5ad0eb88790567a4a0b2d85ce4bd6b191
HeYixuan <15517551511@126.com>
2018-12-28 16:28:43 +0800

    添加

--- Miu-Web/src/main/java/org/igetwell/web/BaseController.java
+++ Miu-Web/src/main/java/org/igetwell/web/BaseController.java
@@ -8,8 +8,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
-import java.io.PrintWriter;
-
 /**
  * 基础类控制器
  */
@@ -35,9 +33,7 @@ public class BaseController {
         try {
             response.get().setCharacterEncoding("UTF-8");
             response.get().setContentType("text/xml; charset=UTF8");
-            PrintWriter out = response.get().getWriter();
-            out.write(renderText);
-            out.flush();
+            response.get().getWriter().print(renderText);
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -47,9 +43,7 @@ public class BaseController {
         try {
             response.get().setCharacterEncoding("UTF-8");
             response.get().setContentType("application/json;charset=UTF-8");
-            PrintWriter out = response.get().getWriter();
-            out.write(renderText);
-            out.flush();
+            response.get().getWriter().write(renderText);
         } catch (IOException e) {
             e.printStackTrace();
         }
